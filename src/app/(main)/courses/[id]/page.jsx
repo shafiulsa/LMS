29589e9d088068a5b5c98c4cf5cb14c301b859a2
shafiulsa <shafiulislam.sa@gmail.com>
2026-09-16 15:13:11 +0600

    mkae testimonial section dynamic

--- src/app/(main)/courses/[id]/page.jsx
+++ src/app/(main)/courses/[id]/page.jsx
@@ -4,6 +4,7 @@ import CourseDetails from "./_components/CourseDetails";
 import Testimonials from "./_components/Testimonials";
 import RelatedCourses from "./_components/RelatedCourses";
 import { getCourseDetails } from "../../../../../queries/courses";
+import { replaceMongoIdInArray } from "@/lib/convertData";
 
 
 const SingleCoursePage = async ({ params }) => {
@@ -22,7 +23,11 @@ const SingleCoursePage = async ({ params }) => {
         thumbnail={course?.thumbnail}
       />
       <CourseDetails />
-      <Testimonials />
+    {/* if have data then display if not then handle this  */}
+      {
+        course?.testimonials && <Testimonials testimonials={replaceMongoIdInArray(course?.testimonials)} />
+      }
+
       <RelatedCourses />
     </>
   );
