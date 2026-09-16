f8d335284b52fd12a7d84a4f453a25db6f793039
shafiulsa <shafiulislam.sa@gmail.com>
2026-09-16 17:24:09 +0600

    make CourseCurriculam section dynamic

--- src/app/(main)/courses/[id]/_components/module/CourseLessonList.jsx
+++ src/app/(main)/courses/[id]/_components/module/CourseLessonList.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Tv } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { getLesson } from '../../../../../../../queries/lessons';
 
-const CourseLessonList = () => {
-    return (
+
+const CourseLessonList = async ({lessonId}) => {
+
+    const lesson = await getLesson(lessonId)
+    // console.log(lesson);
+
+    return ( 
         <div>
 
 <button
@@ -14,22 +20,11 @@ const CourseLessonList = () => {
 >
     <div className="flex items-center gap-x-2">
     <Tv size={16} className={cn("text-slate-500")} />
-    What is React ?
+     {lesson?.title}
     </div>
 </button>
-{/* item ends */}
-{/* item */}
-<button
-    type="button"
-    className={cn(
-    "flex items-center gap-x-2 text-slate-500 text-sm font-[500]  transition-all hover:text-slate-600  w-full"
-    )}
->
-    <div className="flex items-center gap-x-2">
-    <Tv size={16} className={cn("text-slate-500")} />
-    Learn React Basics
-    </div>
-</button>
+ 
+ 
             
         </div>
     );
