15f2ad33d380fe325ba84eb5d2542e18e8e2f717
shafiulsa <shafiulislam.sa@gmail.com>
2026-09-17 09:22:49 +0600

    Make the instructor section dynamic

--- queries/courses.js
+++ queries/courses.js
@@ -4,6 +4,8 @@ import { User } from "../model/user-model";
 import { Testimonial } from "../model/testimonial-model";
 import { Module } from "../model/module-model";
 import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData";
+import { getTestimonialsForCourse } from "./testimonials";
+import { getEnrollmentsForCourse } from "./enrollments";
 
 
 export async function getCourseList() {
@@ -46,3 +48,40 @@ export async function getCourseDetails(id) {
   }).lean();
   return replaceMongoIdInObject(course);
 }  
+
+
+export async function getCourseDetailsByInstructor(instructorId){ 
+  const courses = await Course.find({instructor: instructorId }).lean();
+
+  const enrollments = await Promise.all(
+      courses.map(async (course) => {
+          const enrollment = await getEnrollmentsForCourse(course.
+              _id.toString());
+              return enrollment;
+      }) 
+  );
+
+  const totalEnrollments = enrollments.reduce(( item, currentValue )=> {
+      return item.length + currentValue.length;
+  });
+  
+  const tesimonials = await Promise.all(
+      courses.map(async (course) => {
+          const tesimonial = await getTestimonialsForCourse(course.
+              _id.toString());
+              return tesimonial;
+      })
+  );
+
+  const totalTestimonials = tesimonials.flat();
+  const avgRating = (totalTestimonials.reduce(function (acc, obj) {
+      return acc + obj.rating;
+  },0)) / totalTestimonials.length; 
+
+  return {
+      "courses" : courses.length,
+      "enrollments": totalEnrollments,
+      "reviews" : totalTestimonials.length,
+      "ratings" : avgRating.toPrecision(2)
+  } 
+}
---
+++ queries/testimonials.js
@@ -0,0 +1,8 @@
+import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData"; 
+import { Testimonial } from "../model/testimonial-model";
+
+
+export async function getTestimonialsForCourse(courseId){
+    const testimonials = await Testimonial.find({courseId: courseId}).lean();
+    return replaceMongoIdInArray(testimonials);
+}
